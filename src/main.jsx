2fce079476bad926e033b22c47ebcd2e9e1e865d
ivanvarella <ivanvarella@gmail.com>
2023-07-31 10:32:00 -0300

    Contact component + add new route for it

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,29 @@
+//General imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//React Router
 import Root from './routes/root';
 import ErrorPage from './error-page';
-
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Contact from './routes/Contact';
 
-
+//CSS
 import './index.css'
 
-
+//React Router config
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
